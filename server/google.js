f2b23130384ba39613a96cf3eb0ac8d0b9d17da6
Jagmohan Sharma <152366842+Jagmohansharma20@users.noreply.github.com>
2026-02-28 15:53:14 +0530

    Implement user serialization for Google OAuth
    Added user serialization and deserialization for Google authentication.

--- server/google.js
+++ server/google.js
@@ -1,29 +1,63 @@
 import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
-import jwt from "jsonwebtoken";
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
 
 passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.SERVER_URL ? `${process.env.SERVER_URL}/auth/google/callback` : "/auth/google/callback",
+      callbackURL: process.env.SERVER_URL
+        ? `${process.env.SERVER_URL}/auth/google/callback`
+        : "/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
-
       const user = {
         id: profile.id,
         name: profile.displayName,
         email: profile.emails[0].value,
-        picture: profile.photos[0].value
+        picture: profile.photos[0].value,
       };
-      console.log("Hello");
-      console.log(user);
-      console.log("FULL GOOGLE PROFILE:", profile);
-      console.log("PROFILE PHOTOS:", profile.photos);
       return done(null, user);
     }
   )
 );
 
 export default passport;
+
+// import passport from "passport";
+// import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+// import jwt from "jsonwebtoken";
+
+// passport.use(
+//   new GoogleStrategy(
+//     {
+//       clientID: process.env.GOOGLE_CLIENT_ID,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//       callbackURL: process.env.SERVER_URL ? `${process.env.SERVER_URL}/auth/google/callback` : "/auth/google/callback",
+//     },
+//     async (accessToken, refreshToken, profile, done) => {
+
+//       const user = {
+//         id: profile.id,
+//         name: profile.displayName,
+//         email: profile.emails[0].value,
+//         picture: profile.photos[0].value
+//       };
+//       console.log("Hello");
+//       console.log(user);
+//       console.log("FULL GOOGLE PROFILE:", profile);
+//       console.log("PROFILE PHOTOS:", profile.photos);
+//       return done(null, user);
+//     }
+//   )
+// );
+
+// export default passport;
